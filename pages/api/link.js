febf640587cc2ce989656f889b74d7e41e788ebe
Ben M <benjamin.maheral@gmail.com>
2022-02-22 17:47:07 +0000

    notimeout

--- pages/api/link.js
+++ pages/api/link.js
@@ -3,7 +3,6 @@ import thing from '/config/links.json';
 const code = process.env['code']
 
 export default function handler(req, res) {
-	res.end()
   if (req.method === 'POST') {
 		if (req.body.code === code) {
 			const data = {
