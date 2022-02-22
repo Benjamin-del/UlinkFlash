5a5520ccb8e0ef3bfcba6754bf9dcb902c509b53
Ben M <benjamin.maheral@gmail.com>
2022-02-22 18:40:10 +0000

    Update functions

--- .netlify/functions-internal/link.js
+++ .netlify/functions-internal/link.js
@@ -2,19 +2,33 @@ const fs = require('fs');
 //import thing from '/config/links.json';
 const code = process.env['code']
 
-export default function handler(req, res) {
-  if (req.method === 'POST') {
-		if (req.body.code === code) {
-			const data = {
-				links: req.body.links
-			}
-			const data2write = JSON.stringify(data, null, 2)
-			fs.writeFile('/config/links.json', data2write, (err) => {
-  			if (err)
-    			console.log(err);
-  			else {
-  			}
-			});
+exports.handler = async (event, context) => {
+  // Only allow POST
+  if (event.httpMethod !== "POST") {
+    return { statusCode: 405, body: "Method Not Allowed" };
+  }
+  // queryStringParameters – it’ll be in the event body encoded as a query string
+  const params = querystring.parse(event.body);
+	const dt = fs.readFileSync("config/links.json")
+	const thing = JSON.parse(dt)
+	if (params.code === code) { 
+	const data = {
+		user: thing.user,
+		profile: thing.profile,
+		symbol: thing.symbol,
+		name: thing.name,
+		photo: thing.photo,
+		img_margin: "0",
+		display: thing.display,
+		links: params.links
+	}
+	fs.writeFileSync("config/links.json", data)
+  	return {
+    	statusCode: 200,
+  	};
+	} else {
+		return {
+			statusCode: 405,
 		}
 	}
-}
+};
