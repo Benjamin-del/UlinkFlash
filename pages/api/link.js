9f6e8b74aab1e6bb9e92df5ffe4933fc4016dc55
Ben M <benjamin.maheral@gmail.com>
2022-02-22 17:51:01 +0000

    quick time

--- pages/api/link.js
+++ pages/api/link.js
@@ -6,13 +6,6 @@ export default function handler(req, res) {
   if (req.method === 'POST') {
 		if (req.body.code === code) {
 			const data = {
-				user: thing.user,
-				profile: thing.profile,
-				symbol: thing.symbol,
-				name: thing.name,
-				photo: thing.photo,
-				img_margin: "0",
-				display: thing.display,
 				links: req.body.links
 			}
 			const data2write = JSON.stringify(data, null, 2)
