357e1fe064d9293e687dc6fce4bf2102cc885d57
Ben M <benjamin.maheral@gmail.com>
2022-02-22 16:02:14 +0000

    fix path err

--- pages/api/link.js
+++ pages/api/link.js
@@ -1,7 +1,6 @@
 const fs = require('fs');
-const path = "/public/links.json"
+const path = "../public/links.json"
 const code = process.env['code']
-const dt = require(path)
 
 export default function handler(req, res) {
   if (req.method === 'POST') {
